7eb84bb6b1feb0418c7dcbf267a95ec40fbc86f4
Alexander-Krause <alxander.krause@gmail.com>
2018-01-20 12:31:14 +0000

    wip: clientService use provider

--- src/main/java/net/explorviz/discovery/services/ClientService.java
+++ src/main/java/net/explorviz/discovery/services/ClientService.java
@@ -1,13 +1,20 @@
 package net.explorviz.discovery.services;
 
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
 
 import net.explorviz.discovery.model.Process;
 
@@ -17,19 +24,29 @@ public class ClientService {
 	private static final int HTTP_STATUS_CREATED = 201;
 	private static final String MEDIA_TYPE = "application/vnd.api+json";
 
+	private ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+
+	public <T> void registerProvider(final MessageBodyReader<T> providerReader) {
+		this.clientBuilder = clientBuilder.register(providerReader);
+	}
+
+	public <T> void registerProviderWriter(final MessageBodyWriter<T> providerWriter) {
+		this.clientBuilder = clientBuilder.register(providerWriter);
+	}
+
 	public boolean postProcess(final Process process) {
 		return doPost(process, "http://localhost:8082/process");
 	}
 
-	public boolean doPost(final byte[] payload, final String url) {
-		final Client client = Client.create();
-		final WebResource webResource = client.resource(url);
+	public <T> boolean doPost(final Entity<?> t, final String url) {
+		final Client client = this.clientBuilder.build();
+		final WebTarget webTarget = client.target(url);
 
-		ClientResponse response;
+		Response response;
 
 		try {
-			response = webResource.type(MEDIA_TYPE).post(ClientResponse.class, payload);
-		} catch (UniformInterfaceException | ClientHandlerException e) {
+			response = webTarget.request(MEDIA_TYPE).post(t);
+		} catch (final ProcessingException e) {
 			if (LOGGER.isWarnEnabled()) {
 				LOGGER.warn(
 						"Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}",
@@ -49,40 +66,35 @@ public class ClientService {
 	}
 
 	private boolean doPost(final Process process, final String url) {
-		final Client client = Client.create();
-		final WebResource webResource = client.resource(url);
-
-		ClientResponse response;
-
-		try {
-			response = webResource.type(MEDIA_TYPE).post(ClientResponse.class, process);
-		} catch (UniformInterfaceException | ClientHandlerException e) {
-			if (LOGGER.isWarnEnabled()) {
-				LOGGER.warn(
-						"Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}",
-						url, e.toString());
-			}
-			return false;
-		}
-
-		if (response.getStatus() != HTTP_STATUS_CREATED) {
-			if (LOGGER.isWarnEnabled()) {
-				LOGGER.warn("Failed : HTTP error code: {}", response.getStatus());
-			}
-			return false;
-		}
-
-		return true;
+		/*
+		 * final Client client = Client.create(); final WebResource webResource =
+		 * client.resource(url);
+		 *
+		 * ClientResponse response;
+		 *
+		 * try { response = webResource.type(MEDIA_TYPE).post(ClientResponse.class,
+		 * process); } catch (UniformInterfaceException | ClientHandlerException e) { if
+		 * (LOGGER.isWarnEnabled()) { LOGGER.warn(
+		 * "Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}"
+		 * , url, e.toString()); } return false; }
+		 *
+		 * if (response.getStatus() != HTTP_STATUS_CREATED) { if
+		 * (LOGGER.isWarnEnabled()) { LOGGER.warn("Failed : HTTP error code: {}",
+		 * response.getStatus()); } return false; }
+		 *
+		 * return true;
+		 */
+		return false;
 	}
 
-	public ClientResponse doGETRequest(final String url) {
-		final Client client = Client.create();
-		final WebResource webResource = client.resource(url);
+	public <T> Response doGETRequest(final GenericType<T> t, final String url) {
+		final Client client = this.clientBuilder.build();
+		final WebTarget webTarget = client.target(url);
 
-		ClientResponse response = null;
+		final Response response = null;
 
 		try {
-			response = webResource.accept(MEDIA_TYPE).get(ClientResponse.class);
+			webTarget.request(MEDIA_TYPE).get(t);
 		} catch (UniformInterfaceException | ClientHandlerException e) {
 			if (LOGGER.isWarnEnabled()) {
 				LOGGER.warn(
