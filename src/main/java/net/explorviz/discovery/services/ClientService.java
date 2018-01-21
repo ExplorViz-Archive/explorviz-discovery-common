64b7f679cf1c6b841efe8a4a9b6c426a608f4b76
Alexander-Krause <alxander.krause@gmail.com>
2018-01-21 14:09:52 +0000

    provide GET and POST methods with String response for backend-extension

--- src/main/java/net/explorviz/discovery/services/ClientService.java
+++ src/main/java/net/explorviz/discovery/services/ClientService.java
@@ -5,6 +5,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -17,8 +18,9 @@ import net.explorviz.discovery.model.Process;
 public class ClientService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ClientService.class);
-	private static final int HTTP_STATUS_CREATED = 201;
 	private static final String MEDIA_TYPE = "application/vnd.api+json";
+
+	private static final String LOGGER_MESSAGE = "Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}";
 
 	private final ClientBuilder clientBuilder = ClientBuilder.newBuilder();
 
@@ -43,14 +45,12 @@ public class ClientService {
 			response = client.target(url).request(MEDIA_TYPE).post(Entity.entity(t, MEDIA_TYPE));
 		} catch (final ProcessingException e) {
 			if (LOGGER.isWarnEnabled()) {
-				LOGGER.warn(
-						"Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}",
-						url, e.toString());
+				LOGGER.warn(LOGGER_MESSAGE, url, e.toString());
 			}
 			return false;
 		}
 
-		if (response.getStatus() != HTTP_STATUS_CREATED) {
+		if (response.getStatus() != Response.Status.CREATED.getStatusCode()) {
 			if (LOGGER.isWarnEnabled()) {
 				LOGGER.warn("Failed : HTTP error code: {}", response.getStatus());
 			}
@@ -60,75 +60,68 @@ public class ClientService {
 		return true;
 	}
 
-	private boolean doPost(final Process process, final String url) {
-		/*
-		 * final Client client = Client.create(); final WebResource webResource =
-		 * client.resource(url);
-		 *
-		 * ClientResponse response;
-		 *
-		 * try { response = webResource.type(MEDIA_TYPE).post(ClientResponse.class,
-		 * process); } catch (UniformInterfaceException | ClientHandlerException e) { if
-		 * (LOGGER.isWarnEnabled()) { LOGGER.warn(
-		 * "Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}"
-		 * , url, e.toString()); } return false; }
-		 *
-		 * if (response.getStatus() != HTTP_STATUS_CREATED) { if
-		 * (LOGGER.isWarnEnabled()) { LOGGER.warn("Failed : HTTP error code: {}",
-		 * response.getStatus()); } return false; }
-		 *
-		 * return true;
-		 */
-		return false;
+	public <T> boolean doPost(final String payload, final String url) {
+		final Client client = this.clientBuilder.build();
+
+		Response response;
+
+		try {
+			response = client.target(url).request(MEDIA_TYPE).post(Entity.text(payload));
+		} catch (final ProcessingException e) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn(LOGGER_MESSAGE, url, e.toString());
+			}
+			return false;
+		}
+
+		if (response.getStatus() != Response.Status.CREATED.getStatusCode()) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn("Failed : HTTP error code: {}", response.getStatus());
+			}
+			return false;
+		}
+
+		return true;
 	}
 
-	/*
-	 * public <T> T doGETRequest(final Class<T> type, final String url) { final
-	 * Client client = this.clientBuilder.build();
-	 *
-	 * try { final GenericType<T> genericType = new GenericType<>(type); final
-	 * String r = client.target(url).request(MEDIA_TYPE).get(String.class);
-	 *
-	 * final List<Process> ta = client.target(url).request(MEDIA_TYPE).get(new
-	 * GenericType<List<Process>>() { });
-	 *
-	 * System.out.println(ta); final T t =
-	 * client.target(url).request(MEDIA_TYPE).get(genericType);
-	 * System.out.println(t); return t; } catch (ProcessingException |
-	 * WebApplicationException e) { if (LOGGER.isWarnEnabled()) { LOGGER.warn(
-	 * "Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}"
-	 * , url, e); LOGGER.warn("Stacktrace", e); } }
-	 *
-	 * return null;
-	 *
-	 * /* if (response.getStatus() != HTTP_STATUS_CREATED) { if
-	 * (LOGGER.isWarnEnabled()) { LOGGER.warn("Failed : HTTP error code: {}",
-	 * response.getStatus()); } return false; }
-	 *
-	 * }
-	 */
-
-	public String doGETRequest(final String url) {
+	public <T> T doGETRequest(final Class<T> type, final String url) {
 		final Client client = this.clientBuilder.build();
 
 		try {
-			return client.target(url).request(MEDIA_TYPE).get(String.class);
+			final GenericType<T> genericType = new GenericType<>(type);
+
+			return client.target(url).request(MEDIA_TYPE).get(genericType);
+
 		} catch (ProcessingException | WebApplicationException e) {
 			if (LOGGER.isWarnEnabled()) {
-				LOGGER.warn(
-						"Connection to {} failed, probably not online or wrong IP. Check IP in WEB-INF/classes/explorviz.properties. Error Message: {}",
-						url, e);
+				LOGGER.warn(LOGGER_MESSAGE, url, e);
 				LOGGER.warn("Stacktrace", e);
 			}
 		}
 
 		return null;
+	}
 
-		/*
-		 * if (response.getStatus() != HTTP_STATUS_CREATED) { if
-		 * (LOGGER.isWarnEnabled()) { LOGGER.warn("Failed : HTTP error code: {}",
-		 * response.getStatus()); } return false; }
-		 */
+	public String doGETRequest(final String url) {
+		final Client client = this.clientBuilder.build();
+
+		Response response = null;
+
+		try {
+			response = client.target(url).request(MEDIA_TYPE).get();
+		} catch (ProcessingException | WebApplicationException e) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn(LOGGER_MESSAGE, url, e);
+				LOGGER.warn("Stacktrace", e);
+			}
+			return null;
+		}
+
+		if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+			LOGGER.warn("GET Request failed: {}", response.getStatusInfo());
+		}
+
+		return response.readEntity(String.class);
 	}
 
 }
